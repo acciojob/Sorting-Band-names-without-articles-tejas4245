edc7cf8a17d3b2846bf62b99f2b21f0fce9fc1b3
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-15 10:02:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+let bandList=document.getElementById("band");
+let bandName=["The Beatles","Led Zeppelin","Pink Floyd","The Who","The Rolling Stones","Queen","The Doors"];
+let sortedBandNames=bandNames.sort((a,b)=>){
+let articles=["the","a","an"];
+	let aWithoutArticle=a.toLowerCase().split(" ").filter(word=>!articles.includes(word))[0];
+	let bWithoutArticle=b.toLowerCase().split(" ").filter(word=>!articles.includes(word))[0];
+	return aWithoutArticle.localeCompare(bWithoutArticle);
+})
+sortedBandNames.forEach(name=>{
+	let listItem=document.createElement("li");
+	listItem.textContent=name;
+	bandList.appendChild(listItem);
+});
