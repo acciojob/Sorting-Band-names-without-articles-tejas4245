55b57fd8ca12ac0b5fb4eb2ab11f4b0cf8aff1b7
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-19 15:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 //your code here
-let bandList=document.getElementById("band");
-let bandName=["The Beatles","Led Zeppelin","Pink Floyd","The Who","The Rolling Stones","Queen","The Doors"];
-let sortedBandNames=bandNames.sort((a,b)=>){
-let articles=["the","a","an"];
-	let aWithoutArticle=a.toLowerCase().split(" ").filter(word=>!articles.includes(word))[0];
-	let bWithoutArticle=b.toLowerCase().split(" ").filter(word=>!articles.includes(word))[0];
-	return aWithoutArticle.localeCompare(bWithoutArticle);
-})
-sortedBandNames.forEach(name=>{
-	let listItem=document.createElement("li");
-	listItem.textContent=name;
-	bandList.appendChild(listItem);
+let bandNames=["The Beatles","Rolling Stones","Led Zeppelin","Pink Floyd","The Who","The Doors"];
+function removeArticle(bandName){
+	let articles=["a","an","the"];
+	let words=bandName.split(" ");
+	if(articles.includes(words[0].toLowerCase())){
+		word.shift();
+	}
+	return words.join(" ");
+}
+bandNames.sort(function(a,b){
+	return removeArticle(a).localeCompare(removeArticle(b));
 });
+let ulElement=document.getElementByld("band");
+for(let i=0;i<bandNames.length;i++){
+	let liElement=document.createElement("li");
+	liElement.textContent=bandNames[i];
+	ulElement.appendChild(liElement);
+}
