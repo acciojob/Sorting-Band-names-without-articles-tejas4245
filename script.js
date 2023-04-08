b25995c33054ba89a0bba052e4a12c72f68a92d2
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-04-08 08:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 //your code here
-let bandNames=["The Beatles","Rolling Stones","Led Zeppelin","Pink Floyd","The Who","The Doors"];
-function removeArticle(bandName){
-	let articles=["a","an","the"];
-	let words=bandName.split(" ");
-	if(articles.includes(words[0].toLowerCase())){
-		word.shift();
-	}
-	return words.join(" ");
-}
-bandNames.sort(function(a,b){
-	return removeArticle(a).localeCompare(removeArticle(b));
+let bandNames=['The Beatles','Led Zeppelin','Pink Floyd','The Who','Aerosmith'];
+const articles=['a','an','the'];
+bandNames=bandNames.map(name=>{
+	let newName=name;
+	articles.forEach(article=>{
+		if(newName.toLowerCase().startsWith(article+' ')){
+			newName=newName.slice(article.length+1);
+		}
+	});
+	return newName;
 });
-let ulElement=document.getElementByld("band");
-for(let i=0;i<bandNames.length;i++){
-	let liElement=document.createElement("li");
-	liElement.textContent=bandNames[i];
-	ulElement.appendChild(liElement);
-}
+bandName.sort((a,b)=>a.localCompare(b));
+const bandElement=document.getElementById('band');
+bandName.forEach(name=>{
+	const liElement=document.createElement('li');
+	liElement.appendChild(liElement);
+});
